03616473e65304f0b9aa76670cc3f517a06057d1
jhonatanrlp <jhonatanramosleite21@gmail.com>
2026-06-14 22:57:52 -0300

    site reforma

---
+++ globus/header.js
@@ -0,0 +1,23 @@
+document.getElementById('site-header').innerHTML = `
+  <div class="topbar">
+    <span class="topbar-date">Belém, 14 de junho de 2026</span>
+    <span class="topbar-sep">|</span>
+    <span class="topbar-tag">Edição Digital</span>
+    <span class="topbar-right">globus.esportes</span>
+  </div>
+  <div class="navbar">
+    <div class="navbar-inner">
+      <a class="navbar-brand" href="index.html">
+        <span class="navbar-brand-name">Globus</span>
+        <span class="navbar-brand-sub">Esportes &amp; Notícias</span>
+      </a>
+      <nav class="navbar-nav">
+        <a>Futebol</a>
+        <a>OSM</a>
+        <a>Transferências</a>
+        <a>Clube do Remo</a>
+        <a>Opinião</a>
+      </nav>
+    </div>
+  </div>
+`;
